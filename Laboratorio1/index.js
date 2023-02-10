ef84a4cb447da61d5a82988fe215fe2b89898362
Lina-003 <82280319+Lina-003@users.noreply.github.com>
2023-02-10 18:53:02 -0500

    Lab1

---
+++ Laboratorio1/index.js
@@ -0,0 +1,10 @@
+var numB = require('binary-machine');
+
+console.log(numB('22'));
+console.log(numB('345'));
+
+
+var convert = require('color-convert');
+
+var red = convert.rgb.hsl(25, 68, 200);
+console.log(red);
